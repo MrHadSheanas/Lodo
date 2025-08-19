969532bd89695e2ecc4e651704584b8b747778d1
Fazlahmad <fazlahmadhotaki7@gamil.com>
2025-08-19 02:27:03 -0700

    tired

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let btn = document.querySelector('.btn');
 let img =document.querySelectorAll('.img-one');
 let btn1 = document.querySelector('.btn1');
@@ -7,13 +8,23 @@ let imggood=document.getElementById('good');
 
 
 btn.addEventListener('click', function() { 
+     document.getElementById('dice').style.display ='none'; 
+
 if(collcationone >collcationtwo){
-     imgsad.innerHTML.src='img/good.jpeg';
-     imggood.innerHTML.src='img/sad.jpeg';
+     document.getElementById('sad').src="img/sad.jpeg"
+     document.getElementById('good').src="img/good.jpeg"
+      document.getElementById('star1').src="img/star (1).png"
+         document.getElementById('star2').src="img/star (1).png"
+          document.getElementById('star3').src="img/star (1).png"
+
 }
 else{
-    imggood.innerHTML.src='img/good.jpeg';
-    imgsad.innerHTML.src='img/good.gpeg';
+document.getElementById('good').src ="img/sad.jpeg"
+document.getElementById('sad').src ="img/good.jpeg"
+ document.getElementById('star1');
+ document.getElementById('star2');
+ document.getElementById('star3');
+
 
 
 }
@@ -26,18 +37,21 @@ document.getElementById('span-one').innerText=i;
    document.getElementById('love1').style.display='none';
 
 if(i==6){
-    
+    document.getElementById('love').style.display='block';
+    document.getElementById('love1').style.display='block';
    btn1.style.display='none';
     btn.style.display='block';
      collcationone +=randomindex+1;
-          document.getElementById('love').style.display='block';
 
-
+document.getElementById('div-one').style.transform='scale(1.2)';
    
 }
 else{
-document.getElementById('love').style.display='none';
-    
+      document.getElementById('love').style.display='none';
+    document.getElementById('love1').style.display='none';
+  
+    document.getElementById('div-one').style.transform='scale(1)';
+
      btn.style.display='none';
     btn1.style.display='block';
     collcationone +=randomindex+1;
@@ -55,13 +69,34 @@ let collcationtwo=0;
 
 btn1.addEventListener('click', function() { 
 
+document.getElementById('dice').style.display ='none';
+
+
+if(collcationone >collcationtwo){
+     document.getElementById('sad').src="img/sad.jpeg"
+     document.getElementById('good').src="img/good.jpeg"
+      document.getElementById('star5').src="img/star (1).png"
+         document.getElementById('star4').src="img/star (1).png"
+          document.getElementById('star6').src="img/star (1).png"
+
+}
+else{
+document.getElementById('good').src ="img/sad.jpeg"
+document.getElementById('sad').src ="img/good.jpeg"
+
+
+}
+
 if(collcationone >collcationtwo){
      imgsad.innerHTML.src='img/good.jpeg';
      imggood.innerHTML.src='img/sad.jpeg';
+     
 }
 else{
     imggood.innerHTML.src='img/good.jpeg';
     imgsad.innerHTML.src='img/good.gpeg';
+    
+
 
 
 }
@@ -76,13 +111,14 @@ if(y==6){
  btn.style.display='none';
     btn1.style.display='block';
      collcationtwo +=randomindex+1;
-               document.getElementById('love1').style.display='block';
 
+document.getElementById('div-two').style.transform='scale(1.2)';
 
   
    }
    else{
-   document.getElementById('love1').style.display='none';
+   document.getElementById('div-two').style.transform='scale(1)';
+
 
      collcationtwo +=randomindex+1;
      btn1.style.display='none';
@@ -94,5 +130,4 @@ if(y==6){
     document.getElementById('h-two').innerText=collcationtwo;
 }
 )
-
 
---
+++ second.js
@@ -0,0 +1,15 @@
+let div=document.getElementById('bagest-div');
+let clicking =document.getElementById('clicking');
+let dice =document.getElementById('dice');
+
+clicking.onclick=function() {
+div.style.display='none';
+document.getElementById('p-one').style.display='block';
+document.getElementById('p-two').style.display='block';
+document.getElementById('div-big').style.display='block';
+document.getElementById('div-one').style.display='block';
+document.getElementById('div-two').style.display='block';
+document.getElementById('good').style.display='block';
+document.getElementById('sad').style.display='block';
+
+}
